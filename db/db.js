769f2366cc0e6ab391f53fad638b0823278c6495
Sam Grose <samugrose@gmail.com>
2020-07-13 15:56:56 -0700

    Added method to read notes from file

--- db/db.js
+++ db/db.js
@@ -1,6 +1,17 @@
 const util = require("util");
 const fs = require("fs");
 const path = require("path");
-const jsonDb = (path.resolve(__dirname, "./db.json"));
+const jsond = (path.resolve(__dirname, "./db.json"));
 const readfileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
+
+class dbFunction{
+    async readNotes(){
+        try {
+        const notes = await readfileAsync(jsond, "utf8")
+        return notes ? JSON.parse(notes) : [];
+
+        } catch (err){
+        console.log("Can't read file", err)
+        }
+    }
