5609738eba8a9ea1979fe1e69a24fe2464790a6a
Sam Grose <samugrose@gmail.com>
2020-07-13 15:43:00 -0700

    added server file and post method

--- router.js
+++ router.js
@@ -2,3 +2,18 @@ const { nanoid } = require("nanoid");
 const express = require("express");
 const db = require("./db/db");
 var router = express.Router();
+
+router.get("/notes", async function(req,res){
+    const freshNotes = await db.readNotes();
+    res.json(freshNotes);
+});
+
+//  post method
+router.post('/notes', async function(req,res) {
+    const newNote = req.body;
+    //using nanoid to generate random id for note
+    newNote.id= nanoid();
+    const retrievedNotes = await db.readNotes();
+    const freshlyWrittenNotes = await db.writeNotes([...retrievedNotes, newNote]);
+    res.json(newNote);
+})
