3922505c61636050ac4332c644221650a12ea103
Sam Grose <samugrose@gmail.com>
2020-07-13 15:49:01 -0700

    Added rest of server, listen, etc.

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
-    });
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.use("/api", router);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT http://localhost:" + PORT);
+});
